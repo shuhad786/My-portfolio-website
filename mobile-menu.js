a2b86574f6db396136830951e3b7338cd07463f7
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-05 14:55:59 +0200

    correct linter errors

--- mobile-menu.js
+++ mobile-menu.js
@@ -4,10 +4,9 @@ const menu = document.querySelector('.mobile-menu');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   menu.classList.toggle('active');
-})
+});
 
-document.querySelectorAll('.mobile-nav-link').forEach(
-  n => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    menu.classList.remove('active');
-}))
+document.querySelectorAll('.mobile-nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  menu.classList.remove('active');
+}));
