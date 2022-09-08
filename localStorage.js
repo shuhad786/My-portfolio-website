d9053ff583746be0dc54c4a6ee3ce6b825aa485a
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-08 13:29:37 +0200

    Updated localStorage.js file

--- localStorage.js
+++ localStorage.js
@@ -20,3 +20,16 @@ const reservedata = (element, val) => {
     localStorage.setItem('formObj', JSON.stringify(formData));
   });
 };
+
+reservedata(fname, 'fnameData');
+reservedata(lname, 'lnameData');
+reservedata(name, 'nameData');
+reservedata(email, 'emailData');
+reservedata(messagebox, 'messageData');
+
+const formData = JSON.parse(localStorage.getItem('formObj'));
+fname.value = formData.fnameData;
+lname.value = formData.lnameData;
+email.value = formData.emailData;
+name.value = formData.nameData;
+messagebox.textContent = formData.messageData;
