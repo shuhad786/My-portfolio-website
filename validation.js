5e7cf01986222d88d792ce37f08b6dbf7a8d8fec
jash-debug <jash-debug@github.com>
2022-09-08 11:45:54 +0300

    Create function for validation.js

---
+++ validation.js
@@ -0,0 +1,7 @@
+const submitBtn = document.querySelector('.contact-button');
+const error = document.getElementById('error');
+const validateEmail = (email) => email === email.toLowerCase();
+
+submitBtn.addEventListener('click', () => {
+  const emailVal = document.querySelector('.email').value;
+})
