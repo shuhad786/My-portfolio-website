44a35a806c1cd1528162cb81ea7bd4f230a52337
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-08 21:09:09 +0200

    Made necessary changes for validation

--- validation.js
+++ validation.js
@@ -1,13 +1,6 @@
-const submitBtn = document.querySelector('.contact-button');
+const submitBtn = document.querySelector('.form-button');
+const submitButton = document.querySelector('.desktop-form-button');
 const error = document.getElementById('error');
-const validateEmail = (email) => email === email.toLowerCase();
 
-submitBtn.addEventListener('click', () => {
-  const emailVal = document.querySelector('.email').value;
-  if (!validateEmail(emailVal)) {
-    error.textContent = 'Please enter a valid email address using lower case letters and numbers eg. shuhad@gmail.com';
-    error.style.color = 'red';
-  } else {
-    error.style.display = 'none';
-  }
-});
+
+
