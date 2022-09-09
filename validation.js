21994d3c922e04ec88ae178739b5b773bbdd21fb
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-09 11:05:59 +0200

    Updated validation.js and html with css styles

--- validation.js
+++ validation.js
@@ -1,6 +1,9 @@
 const dEmail = document.getElementById('user-email1');
 const dForm = document.getElementById('form1');
+const Email = document.getElementById('user-email2');
+const Form = document.getElementById('form2');
 const message = document.querySelector('.redColor');
+const message2 = document.querySelector('.redColor2');
 
 dForm.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -14,6 +17,22 @@ dForm.addEventListener('submit', (event) => {
   }
 });
 
-dEmail.addEventListener('focus', () => {
+Email.addEventListener('focus', () => {
+  message2.classList.add('displayNone2');
+});
+
+Form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailInput = Email.value;
+  const emailLowerCase = emailInput.toLowerCase();
+
+  if (emailInput === emailLowerCase) {
+    Form.submit();
+  } else {
+    message2.classList.remove('displayNone2');
+  }
+});
+
+Email.addEventListener('focus', () => {
   message.classList.add('displayNone');
 });
