1a8151c1679038799340e660db3ca581106db509
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-09 12:06:20 +0200

    Updated js file for linter errors

--- local-storage.js
+++ local-storage.js
@@ -1,5 +1,4 @@
 const form = document.querySelector('.form');
-const form2 = document.querySelector('.desktop-form');
 
 const {
   fname, lname, wholeName, Email, Message,
@@ -34,3 +33,38 @@ lname.value = formData.lnameData;
 Email.value = formData.EmailData;
 wholeName.value = formData.wholeNameData;
 Message.textContent = formData.MessageData;
+
+const form2 = document.querySelector('.desktop-form');
+
+const {
+  fname, lname, Email, Message,
+} = form.elements;
+
+if (!localStorage.getItem('formObj')) {
+  const formData = {
+    fnameData: '', lnameData: '', EmailData: '', MessageData: '',
+  };
+
+  localStorage.setItem('formObj', JSON.stringify(formData));
+}
+
+const reservedata2 = (element, val) => {
+  element.addEventListener('change', () => {
+    const data = element.value;
+    const formData2 = JSON.parse(localStorage.getItem('formObj'));
+    formData2[val] = data;
+    localStorage.setItem('formObj', JSON.stringify(formData2));
+  });
+};
+
+reservedata2(fname, 'fnameData');
+reservedata2(lname, 'lnameData');
+reservedata2(Email, 'EmailData');
+reservedata2(Message, 'MessageData');
+
+const formData2 = JSON.parse(localStorage.getItem('formObj'));
+fname.value = formData2.fnameData;
+lname.value = formData2.lnameData;
+Email.value = formData2.EmailData;
+wholeName.value = formData2.wholeNameData;
+Message.textContent = formData2.MessageData;
