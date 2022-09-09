1ec5259490c4538bd15c7840e0a7bc44e83d633c
Shuhad786 <shuhad.loofer@gmail.com>
2022-09-09 12:17:53 +0200

    Update html and js files

--- local-storage.js
+++ local-storage.js
@@ -34,12 +34,12 @@ Message.textContent = formData.MessageData;
 const form2 = document.querySelector('.desktop-form');
 
 const {
-  fname, lname, Email2, Message,
+  fname, lname, Email2, Message2,
 } = form.elements;
 
 if (!localStorage.getItem('formObj')) {
   const formData = {
-    fnameData: '', lnameData: '', EmailData2: '', MessageData: '',
+    fnameData: '', lnameData: '', EmailData2: '', MessageData2: '',
   };
 
   localStorage.setItem('formObj', JSON.stringify(formData));
@@ -57,10 +57,10 @@ const reservedata2 = (element, val) => {
 reservedata2(fname, 'fnameData');
 reservedata2(lname, 'lnameData');
 reservedata2(Email2, 'EmailData2');
-reservedata2(Message, 'MessageData');
+reservedata2(Message2, 'MessageData2');
 
 const formData2 = JSON.parse(localStorage.getItem('formObj'));
 fname.value = formData2.fnameData;
 lname.value = formData2.lnameData;
 Email2.value = formData2.EmailData2;
-Message.textContent = formData2.MessageData;
+Message2.textContent = formData2.MessageData2;
